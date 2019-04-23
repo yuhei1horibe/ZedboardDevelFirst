3c335696a508bfe55441d2d8c6db211991072db9
yuhei <yuhei@yuhei_linux.local>
2019-04-22 22:06:28 -0700

    Removed unnecessary files

--- Driver/zynq_pl/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_drivers/zynq_pl/built-in.o :=  arm-linux-gnueabihf-ld -EL    -r -o drivers/zynq_pl/built-in.o drivers/zynq_pl/my_calculator.o 
--- Driver/zynq_pl/.tmp_versions/my_calculator.mod
+++
@@ -1,3 +0,0 @@
-/home/yuhei/zynq_pl/my_calculator.ko
-/home/yuhei/zynq_pl/my_calculator.o
-
